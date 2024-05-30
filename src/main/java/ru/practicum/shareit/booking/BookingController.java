91fdd25d0b987e1f9d973e53fdc0c49bf126e54b
Marat <xakeponus@yandex.ru>
2024-05-30 21:26:05 +0300

    v0.1

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user;
 
-import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 @Component
