1dbc26fd05c821f72f3694e6f8623247f707bab2
Marat <xakeponus@yandex.ru>
2024-06-19 00:10:27 +0300

    v0.1

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -5,6 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItApp {
-public static void main(String[] args) {
-	SpringApplication.run(ShareItApp.class, args); }
+public static void main(String[] args) {SpringApplication.run(ShareItApp.class, args); }
 }
