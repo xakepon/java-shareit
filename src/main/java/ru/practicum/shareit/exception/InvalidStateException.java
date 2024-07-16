a5b55af596b617e7508485b0af97706a4bac6a5d
Marat <xakeponus@yandex.ru>
2024-07-17 00:33:34 +0300

    v0.4

--- src/main/java/ru/practicum/shareit/exception/InvalidStateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class InvalidStateException extends RuntimeException {
-    public InvalidStateException(final String message) {
-        super(message);
-    }
-}
